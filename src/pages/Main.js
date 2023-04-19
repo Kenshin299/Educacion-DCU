9cf1c823365822fca9b3776afdca4e8add17db33
Kemyl <kemylfernandez299@gmail.com>
2023-04-19 10:15:36 -0400

    Added Toast for Status

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,24 @@
 import AddStudent from "../components/AddStudent";
+import { ToastContainer, toast } from "react-toastify";
 
 function MainPage() {
+    const toasty = () => {
+        toast.success('Enviado con exito', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+    }
+
     return (
         <>
             <AddStudent />
+            {/* <ToastContainer /> */}
         </>
     )
 }

e0067c2295ec2ac0d7788a1cfa34f262281869e1
Kemyl Fernandez <kemylfernandez299@gmail.com>
2023-04-18 23:15:47 -0400

    Readme and Footer Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ function Footer() {
         <footer className="footer">
             <div className="content has-text-centered">
                 <p>
-                    <strong>Educación</strong> by <a href="https://github.com/Kenshin299 " target="_blank" rel="noreferrer">Kemyl Fernandez</a>
+                    <strong>Educación</strong> by <br/>
+                    Kemyl Fernández Bonifacio - 2021-2302 <br/>
+                    Ismelia Marina Jiménez Otaño - 2022-0732 <br/>
+                    Wander Alexis Bautista García - 2021-2444 <br/>
+                    Jan Carlos Gutiérrez 2021-1843 - <br/>
+                    <a href="https://github.com/Kenshin299/Educacion-DCU" target="_blank" rel="noreferrer"> 
+                        <i class="fa-brands fa-github"/> Link de github 
+                    </a>
                 </p>
             </div>
         </footer>
